0c3b6024c9434f235e06825a1860d23029a8369a
Vrushabh Damle <dwrushabh@gmail.com>
2025-10-05 17:11:58 +0530

    Nav bar is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,28 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { HomeIcon } from "@heroicons/react/24/outline";
 export const Navbar = () => {
-  return <div>hello</div>;
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6 ">
+      <div className="flex flex-shrink-0 items-center">
+        <HomeIcon className="h-7 w-7 text-neutral-800 cursor-pointer" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <span className="cursor-pointer">
+          <FaLinkedin></FaLinkedin>
+        </span>
+        <span className="cursor-pointer">
+          <FaGithub></FaGithub>
+        </span>
+        <span className="cursor-pointer">
+          <FaXTwitter></FaXTwitter>
+        </span>
+        <span className="cursor-pointer">
+          <FaInstagram></FaInstagram>
+        </span>
+      </div>
+    </nav>
+  );
 };
