8791525dd5217c1997bc5e78c77f495beccd91df
Vrushabh Damle <dwrushabh@gmail.com>
2025-10-06 17:10:25 +0530

    ready to deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,31 @@
-import { AiFillStar } from "react-icons/ai";
+import { FaStar } from "react-icons/fa";
 import { motion } from "motion/react";
+
 export const Footer = () => {
   return (
-    <div className="pb-4">
-      <div className="flex flex-wrap justify-center text-neutral-800 my-5 text-center text-4xl ">
-        {" "}
-        <div className="bg-neutral-900 text-neutral-50 p-3 rounded-4xl">
-          <AiFillStar className="w-8 h-8" />
-        </div>
+    <div>
+      <div className="flex justify-center ">
+        <a
+          href="https://github.com/Vrushabh-Damle/Portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative inline-flex items-center justify-center rounded-full bg-neutral-800 p-3 text-white transition-colors group mt-4"
+        >
+          <FaStar className="text-xl" />
+
+          {/* Tooltip */}
+          <span className="absolute bottom-12 left-1/2 -translate-x-1/2 scale-0 rounded-md bg-neutral-800 px-3 py-1 text-sm text-white opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100">
+            Star this repo
+          </span>
+        </a>
       </div>
-      <div className="text-center text-neutral-600 font-light text-xl mt-5">
-        <i>Made with ❤️ by Vrushabh Damle</i>
+      <div className="flex justify-center">
+        <div className="mb-6 mt-4">
+          <i className="text-xl font-light text-neutral-600 ">
+            {" "}
+            Made with ❤️ by Vrushabh Damle
+          </i>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,57 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { HomeIcon } from "@heroicons/react/24/outline";
 export const Navbar = () => {
+  const handleHomeClick = () => {
+    window.location.reload();
+  };
   return (
     <nav className="mb-20 flex items-center justify-between py-6 ">
       <div className="flex flex-shrink-0 items-center">
-        <HomeIcon className="h-7 w-7 text-neutral-800 cursor-pointer" />
+        <HomeIcon
+          className="h-7 w-7 text-neutral-800 cursor-pointer"
+          onClick={handleHomeClick}
+        />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl text-neutral-800">
         <span className="cursor-pointer">
-          <FaLinkedin></FaLinkedin>
+          <a
+            href="https://www.linkedin.com/in/vrushabh-damle-2a1817216/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer hover:text-blue-700 transition-colors"
+          >
+            <FaLinkedin />
+          </a>
         </span>
         <span className="cursor-pointer">
-          <FaGithub></FaGithub>
+          <a
+            href="https://github.com/Vrushabh-Damle"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer hover:text-gray-700 transition-colors"
+          >
+            <FaGithub />
+          </a>
         </span>
         <span className="cursor-pointer">
-          <FaXTwitter></FaXTwitter>
+          <a
+            href="https://x.com/vrushabhstwtt"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer hover:text-black transition-colors"
+          >
+            <FaXTwitter />
+          </a>
         </span>
         <span className="cursor-pointer">
-          <FaInstagram></FaInstagram>
+          <a
+            href="https://www.instagram.com/vrushabh_damle/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cursor-pointer hover:text-pink-600 transition-colors"
+          >
+            <FaInstagram />
+          </a>
         </span>
       </div>
     </nav>
